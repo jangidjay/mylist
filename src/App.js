850a03e098b6936cbc741c8efc7de9be0df3dfa6
jangidjay <jangid321jay@gmail.com>
2022-04-05 07:34:56 +0530

    Starters

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import './App.css';
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import Home from "./Components/Pages/Home";
+import SignUp from "./Components/Pages/Signup";
+import AboutUs from "./Components/Pages/Aboutus";
+import Account from "./Components/Pages/Account";
+import Anime from "./Components/Pages/Anime";
+import Movies from "./Components/Pages/Movies";
+import Series from "./Components/Pages/Series";
 
 function App() {
   return (
-    <div className="App">
-      <h1>My List</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/aboutus" element={<AboutUs />} />
+          <Route path="/account" element={<Account />} />
+          <Route path="/anime" element={<Anime />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/series" element={<Series />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
---
+++ src/Components/Navbar.js
@@ -0,0 +1,98 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Button } from "./Button";
+import "./Navbar.css";
+
+function Navbar() {
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+            MYLIST <i className="fab fa-typo3"></i>
+          </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"} />
+          </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/anime" className="nav-links" onCLick={closeMobileMenu}>
+                Anime
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/movies"
+                className="nav-links"
+                onCLick={closeMobileMenu}
+              >
+                Movies
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/series"
+                className="nav-links"
+                onCLick={closeMobileMenu}
+              >
+                Series
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/account"
+                className="nav-links"
+                onCLick={closeMobileMenu}
+              >
+                Account
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/aboutus"
+                className="nav-links"
+                onCLick={closeMobileMenu}
+              >
+                AboutUs
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/signup"
+                className="nav-links"
+                onCLick={closeMobileMenu}
+              >
+                SignUp
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
