21f47161bfe4a3d221b6a3edf0901d5bfd68fa9d
jangidjay <jangid321jay@gmail.com>
2022-04-12 16:13:28 +0530

    Banner

--- src/Components/FrontBanner.js
+++ src/Components/FrontBanner.js
@@ -1,7 +1,32 @@
 import React from "react";
+import "../App.css";
+import { Button } from "./Button";
+import "./FrontBanner.css";
 
-function FrontBanner() {
-  return <div>FrontBanner</div>;
+function HeroSection() {
+  return (
+    <div className="hero-container">
+      {/* <div className="background-img">jid</div> */}
+      <h1>ADVENTURE AWAITS</h1>
+      <p>What are you waiting for?</p>
+      <div className="hero-btns">
+        <Button
+          className="btns"
+          buttonStyle="btn--outline"
+          buttonSize="btn--large"
+        >
+          GET STARTED
+        </Button>
+        <Button
+          className="btns"
+          buttonStyle="btn--primary"
+          buttonSize="btn--large"
+        >
+          WATCH TRAILER <i className="far fa-play-circle" />
+        </Button>
+      </div>
+    </div>
+  );
 }
 
-export default FrontBanner;
+export default HeroSection;
