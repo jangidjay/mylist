698e6ed1d86406d9755b94336825e24728dbfa8b
jangidjay <jangid321jay@gmail.com>
2022-04-15 19:18:49 +0530

    build

---
+++ build/precache-manifest.e2d1aa03da549e4c63301383fb93b573.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "df1c2b970c67a6a2a49d",
+    "url": "/static/css/main.4b7806d0.chunk.css"
+  },
+  {
+    "revision": "df1c2b970c67a6a2a49d",
+    "url": "/static/js/main.df1c2b97.chunk.js"
+  },
+  {
+    "revision": "191cd51e9e2aa3b7d32b",
+    "url": "/static/js/1.191cd51e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "18de4955f438cb606d601093e04bc26a",
+    "url": "/index.html"
+  }
+];
